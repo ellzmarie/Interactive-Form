f569086a3913e9ccac2f88e9fc4ebb0ab911230e
Ella <elliematutisdigal@gmail.com>
2021-10-28 16:53:55 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ window.onload = function () {
     document.getElementById("name").focus();
 }
 
+// The "Job Role" section
+// Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.
+
+
+// Program the "Job Role" <select> element to listen for user changes. 
+// When a change is detected, display/hide the "text field" based on the user’s selection in the drop down menu.
