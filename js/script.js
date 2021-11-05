b982f25f0fd81710402909b72e5ff7fecf697372
Ella <elliematutisdigal@gmail.com>
2021-11-05 10:36:54 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,17 @@ const colorTheme = document.getElementById('color');
 const colorThemeOption = colorTheme.children;
 colorTheme.disabled = true;
 
+designTheme.addEventListener ('change', e=> {
+    colorTheme.disabled = false;
+    for (let i=0; i < colorThemeOption.length; i++){
+        const selectTheme = e.target.value;
+        const themeOption = colorThemeOption[i].getAttribute('data-theme');
+        if (selectTheme === themeOption) {
+            colorOption[i].hidden = false;
+            colorOption[i].setAttribute('selected', true);
+        } else {
+            colorOption[i].hidden = true;
+            colorOption[i].removeAttribute('selected');
+        }
+    }
+});
